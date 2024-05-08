f08a70a98b91d3ebcbccab5cd3e522728a335a89
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-05-08 14:09:47 -0600

    added a speed display

---
+++ Assets/Scripts/Prototype/SpeedReader.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+
+	public class SpeedReader : MonoBehaviour
+	{
+		[SerializeField] private Rigidbody carRigidbody;
+
+		[SerializeField] private TMP_Text speedTextBox;
+
+		private void Update()
+		{
+			speedTextBox.text = Vector3.Dot(carRigidbody.velocity, carRigidbody.transform.forward).ToString();
+		}
+	}
