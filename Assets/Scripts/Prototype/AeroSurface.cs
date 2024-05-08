3f05f9b8cce6090086838824829b3d662277856d
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-05-08 13:11:28 -0600

    added a script to reset

---
+++ Assets/Scripts/Prototype/AeroSurface.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+	public class AeroSurface : MonoBehaviour
+	{
+		private void FixedUpdate()
+		{
+			
+		}
+	}
---
+++ Assets/Scripts/Prototype/Reset.cs
@@ -0,0 +1,28 @@
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Reset : MonoBehaviour
+	{
+		[SerializeField] private Transform carTransform;
+		private Rigidbody carRigidbody;
+
+		private Vector3 startLocation;
+
+		private void Start()
+		{
+			startLocation = carTransform.position;
+			carRigidbody = carTransform.GetComponent<Rigidbody>();
+		}
+
+		public void ResetPosition(InputAction.CallbackContext context)
+		{
+			if(context.performed)
+			{
+				carTransform.position = startLocation;
+				carTransform.rotation = Quaternion.identity;
+				carRigidbody.velocity = Vector3.zero;
+				carRigidbody.angularVelocity = Vector3.zero;
+			}
+		}	
+	}
