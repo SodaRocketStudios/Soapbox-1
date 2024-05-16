da79d2e112af46877786296c61bc68605354caf6
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-05-16 13:49:17 -0600

    added lateral curve and made curves populate on asset creating

--- Assets/Scripts/Physics/CurveTireProfile.cs
+++ Assets/Scripts/Physics/CurveTireProfile.cs
@@ -7,22 +7,39 @@ namespace Soap.Physics
 	public class CurveTireProfile : ScriptableObject
 	{
 		[Header("Longitudinal Parameters")]
-		[SerializeField, Range(1, 2)] private float peakFriction;
-		[SerializeField, Range(0.01f, 0.99f)] private float peakSlip;
-		[SerializeField, Range(1, 2)] private float slipFriction;
-		[SerializeField] private AnimationCurve longitudinalCurve;
+		[SerializeField, Range(1, 3)] private float peakLongitudinalFriction = 1.75f;
+		[SerializeField, Range(0.01f, 0.99f)] private float peakSlipRatio = 0.1f;
+		[SerializeField, Range(1, 3)] private float longitudinalSlipFriction = 1.5f;
+		[SerializeField] private AnimationCurve longitudinalCurve = new AnimationCurve();
 
-		[SerializeField] private AnimationCurve lateralCurve;
+		[Header("Lateral Parameters")]
+		[SerializeField, Range(1, 3)] private float peakLateralFriction = 1.65f;
+		[SerializeField, Range(0.1f, 40)] private float peakSlipAngle = 7.5f;
+		[SerializeField, Range(1, 3)] private float lateralSlipFriction = 1.35f;
+		[SerializeField] private AnimationCurve lateralCurve = new AnimationCurve();
+
+		private void Awake()
+		{
+			OnValidate();
+		}
 
 		private void OnValidate()
 		{
+			// Longitudinal Curve
 			longitudinalCurve.ClearKeys();
 			longitudinalCurve.AddKey(0, 0);
-			longitudinalCurve.AddKey(peakSlip, peakFriction);
-			longitudinalCurve.AddKey(1, slipFriction);
-			AnimationUtility.SetKeyBroken(longitudinalCurve, 0, true);
+			longitudinalCurve.AddKey(peakSlipRatio, peakLongitudinalFriction);
+			longitudinalCurve.AddKey(1, longitudinalSlipFriction);
 			AnimationUtility.SetKeyRightTangentMode(longitudinalCurve, 0, AnimationUtility.TangentMode.Linear);
 			AnimationUtility.SetKeyRightTangentMode(longitudinalCurve, 1, AnimationUtility.TangentMode.ClampedAuto);
+
+			// Lateral Curve
+			lateralCurve.ClearKeys();
+			lateralCurve.AddKey(0, 0);
+			lateralCurve.AddKey(peakSlipAngle, peakLateralFriction);
+			lateralCurve.AddKey(1, lateralSlipFriction);
+			AnimationUtility.SetKeyRightTangentMode(lateralCurve, 0, AnimationUtility.TangentMode.Linear);
+			AnimationUtility.SetKeyRightTangentMode(lateralCurve, 1, AnimationUtility.TangentMode.ClampedAuto);
 		}
 	}
 }
