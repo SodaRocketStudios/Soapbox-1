222165963ed3392b1e6e4dd401c5d53a3fa8a703
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-05-22 10:08:28 -0600

    Added debug lines to aero

--- Assets/Scripts/Prototype/ProtoAeroSurface.cs
+++ Assets/Scripts/Prototype/ProtoAeroSurface.cs
@@ -25,8 +25,10 @@ namespace Soap.Prototype
 			float longitudinalVelocitySquared = longitudinalVelocity * longitudinalVelocity;
 
 			float dragForce = longitudinalVelocitySquared*frontalArea*dragCoefficient*AIR_DENSITY/2;
+			Debug.DrawRay(transform.position, transform.forward*dragForce, Color.red);
 
 			float liftForce = longitudinalVelocitySquared*frontalArea*dragCoefficient*liftToDragRatio*AIR_DENSITY/2;
+			Debug.DrawRay(transform.position, transform.up*liftForce, Color.magenta);
 
 			carRigidBody.AddForceAtPosition(-dragForce*transform.forward, transform.position);
 			carRigidBody.AddForceAtPosition(-liftForce*transform.up, transform.position);
