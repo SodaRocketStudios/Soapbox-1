aefe436693fb9c68914a0e336bd2413bb4172ac8
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-06-06 12:35:41 -0600

    changed updater to only be created if a callback is added

--- Assets/Scripts/SRS/Utils/Updater.cs
+++ Assets/Scripts/SRS/Utils/Updater.cs
@@ -5,45 +5,23 @@ namespace SRS.Utils
 {
 	public class Updater : MonoBehaviour
 	{
-		private Updater instance;
-		public Updater Instance
+		private static Updater instance;
+
+		private Action<float> updateCallback;
+
+		public static void AddUpdateCallback(Action<float> updateMethod)
 		{
-			get
+			if (instance == null)
 			{
-				if(instance == null)
-				{
-					CreateUpdater();
-				}
-
-				return instance;
+				instance = new GameObject("[Update Caller]").AddComponent<Updater>();
 			}
-		}
 
-		public static event Action<float> OnUpdate;
-
-		private void CreateUpdater()
-		{
-			instance = new GameObject("Updater").AddComponent<Updater>();
+			instance.updateCallback += updateMethod;
 		}
 
 		private void Update()
 		{
-			OnUpdate?.Invoke(Time.deltaTime);
+			updateCallback(Time.deltaTime);
 		}
 	}
-
-	// public class UpdateCaller : MonoBehaviour {
-    // private static UpdateCaller instance;
-    // public static void AddUpdateCallback(Action updateMethod) {
-    //     if (instance == null) {
-    //         instance = new GameObject("[Update Caller]").AddComponent<UpdateCaller>();
-    //     }
-    //     instance.updateCallback += updateMethod;
-    // }
- 
-    // private Action updateCallback;
- 
-    // private void Update() {
-    //     updateCallback();
-    // }
 }
