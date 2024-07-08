e0c8127aa5e0b660b264ea065c6e12d2e6d03508
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-07-08 10:37:08 -0600

    started timing box script

---
+++ Assets/Scripts/UI/TimingBox.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using TMPro;
+
+namespace Soap.UI
+{
+	public class TimingBox : MonoBehaviour
+	{
+		[SerializeField] private TMP_Text deltaTextBox;
+		[SerializeField] private TMP_Text bestTextBox;
+		[SerializeField] private TMP_Text currentTextBox;
+		[SerializeField] private TMP_Text Sector1TextBox;
+		[SerializeField] private TMP_Text Sector2TextBox;
+		[SerializeField] private TMP_Text Sector3TextBox;
+	}
+}
