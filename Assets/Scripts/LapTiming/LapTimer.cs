aa7ef60421c82ca3d5929a0de199af5d0c42a8cf
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-07-09 09:15:18 -0600

    Refectored timing

---
+++ Assets/Scripts/LapTiming/LapTimer.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+using SRS.Utils.Timing;
+
+namespace Soap.LapTiming
+{
+	public class LapTimer : MonoBehaviour
+	{
+		public Action<float> OnTimeChanged;
+
+		public TimedSegment Lap {get; private set;}
+		public TimedSegment[] Sectors {get; private set;} = new TimedSegment[3];
+
+		private Timer timer;
+
+		private float sectorStartTime = 0;
+
+		private void Awake()
+		{
+			timer = new();
+		}
+
+		private void Start()
+		{
+			timer.Time.OnChange = OnTimeChanged;
+		}
+
+		public void StartTimer()
+		{
+			timer.Reset();
+			timer.Start();
+			sectorStartTime = 0;
+		}
+
+		public void LogLapTime()
+		{
+			timer.Pause();
+			Lap.LogTime(timer.Time);
+		}
+
+		public void LogSectorTime(int sectorIndex)
+		{
+			Sectors[sectorIndex - 1].LogTime(timer.Time - sectorStartTime);
+			sectorStartTime = Sectors[sectorIndex - 1].LastTime;
+		}
+
+		public void UpdateDelta()
+		{
+			
+		}
+	}
+}
---
+++ Assets/Scripts/LapTiming/TimedSegment.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+namespace Soap.LapTiming
+{
+	public class TimedSegment : MonoBehaviour
+	{
+		public Action OnNewBest; //TODO -- Do I need to notify every time a time is recorded to set colors on UI?
+		public Action<int> OnTimeLogged; // Passes 1 if a new best was set and -1 otherwise.
+
+		public float BestTime {get; private set;} = -1;
+		public float LastTime {get; private set;} = -1;
+
+		public void LogTime(float time)
+		{
+			LastTime = time;
+
+			if(BestTime <= 0 || time < BestTime)
+			{
+				BestTime = time;
+				OnNewBest?.Invoke();
+				OnTimeLogged?.Invoke(1);
+				return;
+			}
+
+			OnTimeLogged?.Invoke(-1);
+		}
+	}
+}
