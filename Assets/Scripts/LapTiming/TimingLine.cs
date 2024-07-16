dcd485830a07cd6e4c1e92f02ed1905ba1494017
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-07-16 09:35:13 -0600

    replaced prototype timing lines

--- Assets/Scripts/LapTiming/TimingLine.cs
+++ Assets/Scripts/LapTiming/TimingLine.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Soap.LapTiming
 {
 	public class TimingLine : MonoBehaviour
 	{
-		
+		public UnityEvent OnEnter;
+
+		private void OnTriggerEnter(Collider other)
+		{
+			OnEnter.Invoke();
+		}
 	}
 }
