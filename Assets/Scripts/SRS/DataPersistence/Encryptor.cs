2daa56d99412782b33e0a7d2e2a1812ea71c82e6
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-09-25 11:30:58 -0600

    created encryption system

--- Assets/Scripts/SRS/DataPersistence/Encryptor.cs
+++ Assets/Scripts/SRS/DataPersistence/Encryptor.cs
@@ -1,18 +1,69 @@
+using System;
 using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
 
 namespace SRS.DataPersistence
 {
 	public static class Encryptor
 	{
-		private static Aes aes = Aes.Create();
+		private static byte[] key = new byte[16];
+		private static byte[] iv = new byte[16];
 
-		public static void Encrypt(string data)
+		public static string Encrypt(string data)
 		{
+			GenerateIV();
+			GenerateKey();
+
+			SymmetricAlgorithm algorithm = Aes.Create();
+			ICryptoTransform transform = algorithm.CreateEncryptor(key, iv);
+
+			byte[] inputBuffer = Encoding.Unicode.GetBytes(data);
+			byte[] outputBuffer = transform.TransformFinalBlock(inputBuffer, 0, inputBuffer.Length);
+
+			string ivString = Encoding.Unicode.GetString(iv);
+			string encryptedString = Convert.ToBase64String(outputBuffer);
+
+			return ivString + encryptedString;
 		}
 
-		public static string Decrypt()
+		public static string Decrypt(string encryptedData)
 		{
-			return null;
+			GenerateIV();
+			GenerateKey();
+
+			int endOfIV = iv.Length / 2;
+
+			string ivString = encryptedData.Substring(0, endOfIV);
+			byte[] extractedIV = Encoding.Unicode.GetBytes(ivString);
+
+			string encryptedString = encryptedData.Substring(endOfIV);
+
+			SymmetricAlgorithm algorithm = Aes.Create();
+			ICryptoTransform transform = algorithm.CreateEncryptor(key, extractedIV);
+
+			byte[] inputBuffer = Convert.FromBase64String(encryptedString);
+			byte[] outputBuffer = transform.TransformFinalBlock(inputBuffer, 0, inputBuffer.Length);		
+
+			return Encoding.Unicode.GetString(outputBuffer);
+		}
+
+		private static void GenerateKey()
+		{
+			int sum = 0;
+			foreach(char character in Application.productName)
+			{
+				sum += character;
+			}
+
+			System.Random rnd = new System.Random(sum);
+			rnd.NextBytes(key);
+		}
+
+		private static void GenerateIV()
+		{
+			System.Random rnd = new System.Random();
+			rnd.NextBytes(iv);
 		}
 	}
 }
